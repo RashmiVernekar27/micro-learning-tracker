749b8875b6ddca5c310687e8d7444e35254d3ae7
RashmiVernekar27 <www.vernekarrashmi@gmail.com>
2026-05-25 14:40:45 +0000

    ython

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -7,4 +7,4 @@ def home():
     return "micro learning engine: Online and Tracking!"
 
 if __name__ == '_main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
