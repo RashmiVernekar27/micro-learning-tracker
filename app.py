aa490d5b3683c528a570e2182f0f189b66bd1f87
RashmiVernekar27 <www.vernekarrashmi@gmail.com>
2026-05-25 14:53:38 +0000

    fix synatx errors and successfully run Flask server on port 5000

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,5 +7,5 @@ app = Flask(__name__)
 def home():
     return "micro learning engine: Online and Tracking!"
 
-if __name__ == '_main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
