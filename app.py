ea2f8ffa9ef3d319013133c9357fa023ef38a4bd
RashmiVernekar27 <www.vernekarrashmi@gmail.com>
2026-05-25 14:16:46 +0000

    Initial project setup with Flask backend

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "micro learning engine: Online and Tracking!"
+
+if __name__ == '_main__':
+    app.run(host='0.0.0.0', port=5000,debug=True)
